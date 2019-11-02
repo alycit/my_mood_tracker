b883d377ede18ca8ef2b0f68582f610474e7755f
Alyssa Diaz <alyssa.diaz@stitchfix.com>
2019-11-02 13:41:42 -0500

    Finish login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'auth/auth0/callback' => 'auth0#callback'
   get 'auth/failure' => 'auth0#failure'
+  get '/logout' => 'logout#logout'
+  
+  get 'dashboard' => 'dashboard#show'
+
+  root 'home#show'
 end
